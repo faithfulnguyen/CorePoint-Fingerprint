628c53e9809d13586f15d280915b1bc2367786c4
trungtinnguyen <trungtinnguyen2894@gmail.com>
2016-11-11 04:48:50 +0700

    it's look like stupid, but it's run, it's not stupid, I don't know how it run

--- src/corepoint/CorePoint.java
+++ src/corepoint/CorePoint.java
@@ -6,8 +6,6 @@
 package corepoint;
 
 import java.io.File;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.Arrays;
 import org.bytedeco.javacpp.indexer.DoubleRawIndexer;
 import org.bytedeco.javacpp.indexer.FloatRawIndexer;
@@ -17,8 +15,11 @@ import static org.bytedeco.javacpp.opencv_core.BORDER_DEFAULT;
 import static org.bytedeco.javacpp.opencv_core.CV_32F;
 import static org.bytedeco.javacpp.opencv_core.CV_PI;
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.Point;
+import org.bytedeco.javacpp.opencv_core.Scalar;
 import org.bytedeco.javacpp.opencv_core.Size;
 import static org.bytedeco.javacpp.opencv_core.print;
+import org.bytedeco.javacpp.opencv_highgui;
 import org.bytedeco.javacpp.opencv_imgcodecs;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imwrite;
@@ -44,27 +45,28 @@ public class CorePoint {
         Arrays.sort(listOfFiles);
         for(int i = 0; i < listOfFiles.length; i++){
             if (listOfFiles[i].getName().contains(".tif")){
-                opencv_core.Mat img = imread (fileName + "/"+ listOfFiles[i].getName(), opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+                //opencv_core.Mat img = imread ("src/corepoint/img.jpg", opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+                String name =  listOfFiles[i].getName();
+                opencv_core.Mat img = imread (fileName + name, opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
                 resize(img, img, new Size(120,120));
                 Mat normailizedImg = normalizeSubWindow(img);
                 CorePoint cr = new CorePoint();
                 Mat[] ori = cr.localSmooth(normailizedImg);
                 Mat smooth = cr.smoothedOrientation(ori[0], ori[1]);
-                cr.poinCare(smooth, img, listOfFiles[i].getName());
+                cr.poinCare(smooth, normailizedImg, listOfFiles[i].getName());
             }
-        }
-      
-            
+        }   
     }
    
     public static opencv_core.Mat normalizeSubWindow(opencv_core.Mat image){
         int ut = 139;
         int vt = 100;//1607;  
-        double u = meanMatrix(image);
-        double v = variance(image, u);
-        UByteRawIndexer idx = image.createIndexer();
-        for(int i = 0; i < image.rows(); i++){
-            for(int j = 0; j < image.cols() ; j++){
+        Mat img = image.clone();
+        double u = meanMatrix(img);
+        double v = variance(img, u);
+        UByteRawIndexer idx = img.createIndexer();
+        for(int i = 0; i < img.rows(); i++){
+            for(int j = 0; j < img.cols() ; j++){
                 double beta = Math.sqrt((vt * 1.0 / v ) * (Math.pow(idx.get(i, j) - u, 2)));
                 if(idx.get(i, j) > ut){
                     idx.put(i, j, 0, (int)ut + (int)beta);
@@ -72,7 +74,7 @@ public class CorePoint {
                 else idx.put(i, j, 0, Math.abs((int)ut - (int)beta));      
             }
         }
-        return image;
+        return img;
     }
    
     public static double variance(opencv_core.Mat image, double mean){
@@ -118,7 +120,7 @@ public class CorePoint {
     
     public float sobelDerivativeX(int x, int y, Mat img){
         UByteRawIndexer idx = img.createIndexer();
-        return -idx.get(x - 1, y - 1) - 2 * idx.get(x, y - 1) - idx.get(x + 1, y -1)
+        return -idx.get(x - 1, y - 1) - 2 * idx.get(x, y - 1) - idx.get(x + 1, y - 1)
                 + idx.get(x - 1, y + 1) + 2 * idx.get(x, y + 1) + idx.get(x + 1, y + 1);
                 
     }
@@ -129,7 +131,7 @@ public class CorePoint {
                 + idx.get(x + 1, y - 1) + 2*idx.get(x + 1, y) + idx.get(x + 1, y + 1);
     }
     
-    public double blockOrientation( Mat blck_sbl_x, Mat blck_sbl_y){
+    public double blockOrientation( Mat blck_sbl_x, Mat blck_sbl_y) {
     	double v_x = 0;
     	double v_y = 0;
         int w = blck_sbl_x.rows();
@@ -141,7 +143,7 @@ public class CorePoint {
                 v_y += (idx_x.get(u, v)*idx_x.get(u, v) - idx_y.get(u, v)*idx_y.get(u, v));
             }
     	}
-    	double theta = 0.5 * ( Math.PI + Math.atan2(v_x, v_y));
+    	double theta = 0.5 * (Math.PI + Math.atan2(v_x, v_y));
     	return theta;
     }
 
@@ -170,44 +172,41 @@ public class CorePoint {
     public Mat[] localSmooth(Mat img){
     	Mat[] sb = sobelDerivatives(img);
     	Mat ori = localOrientation(img, sb[0], sb[1]);
-    	Mat sinY = new Mat(ori.size(), ori.type());
-    	Mat cosX = new Mat(ori.size(), ori.type());
-    	DoubleRawIndexer idx_o = ori.createIndexer();
+    	Mat sinY = ori.clone();
+    	Mat cosX = ori.clone();
     	DoubleRawIndexer idx_x = cosX.createIndexer();
     	DoubleRawIndexer idx_y = sinY.createIndexer();
     	for(int i = 0; i < ori.rows(); i++){
             for(int j = 0; j < ori.cols(); j++){
-                idx_x.put(i, j, Math.cos(2 * idx_o.get(i, j)));
-                idx_y.put(i, j, Math.sin(2 * idx_o.get(i, j)));
+                idx_x.put(i, j, Math.cos(2 * idx_x.get(i, j)));
+                idx_y.put(i, j, Math.sin(2 * idx_y.get(i, j)));
             }
     	}
+       
     	return new Mat[]{ cosX, sinY};
     }
 
     public Mat smoothedOrientation(Mat cosX, Mat sinY){
         GaussianBlur(cosX, cosX, new Size(3, 3), 0, 0, BORDER_DEFAULT);
         GaussianBlur(sinY, sinY, new Size(3, 3), 0, 0, BORDER_DEFAULT);
-        Mat smooth = cosX.clone();
+        Mat smooth = new Mat(cosX.size(), cosX.type());
         DoubleRawIndexer idx_s = smooth.createIndexer();
-        DoubleRawIndexer idx_x =  cosX.createIndexer();
+        DoubleRawIndexer idx_x = cosX.createIndexer();
         DoubleRawIndexer idx_y = sinY.createIndexer();
         for(int i = 0; i < smooth.rows(); i++){
             for(int j = 0; j < smooth.cols(); j++){
-                double theta = 0.5 * Math.atan2(idx_y.get(i, j), idx_x.get(i,j));
-                idx_s.put(i, j, theta);
+                double theta =  Math.atan2(idx_y.get(i, j), idx_x.get(i, j)) / 2;
+                idx_s.put(j, i, theta);
             }
         }
     	return smooth;
     }
+   
     public void poinCare(Mat smooth, Mat img, String name){
         Mat siglar = smooth.clone();
     	DoubleRawIndexer index = siglar.createIndexer();
         Mat rgb = new Mat();
-        opencv_imgproc.cvtColor(img, rgb, CV_GRAY2RGB );
-        UByteRawIndexer idxI = rgb.createIndexer();
-        int w = 3;
-        int i =  smooth.rows();
-        int j =  smooth.cols();
+        opencv_imgproc.cvtColor(img, rgb, CV_GRAY2RGB);
     	for(int r = 1; r < smooth.rows() - 1; r ++){
             for(int c = 1; c < smooth.cols() - 1; c ++){
                 double beta = calcNeighbors(smooth, r, c);
@@ -216,13 +215,8 @@ public class CorePoint {
     	}
         for(int r = 0; r < siglar.rows(); r++){
             for(int c = 0; c < siglar.cols(); c++){
-                if(index.get(r, c) >= 0.50 && index.get(r, c) <= 0.51){
-                    for(int k = 0; k < 10; k++){
-                        idxI.put(c*10 + k, r*10 + k, 0, 150);
-                        idxI.put(c*10 + k, r*10 + k, 1, 0);
-                        idxI.put(c*10 + k, r*10 + k, 2, 0);
-                    }
-                    
+                if(index.get(r, c) == 1.0){
+                    opencv_imgproc.circle(rgb,new Point(c * 10, r *10), 3, org.bytedeco.javacpp.helper.opencv_core.AbstractScalar.CYAN);
                 }
             }
         }
@@ -230,42 +224,58 @@ public class CorePoint {
     }
  
     public double calcNeighbors(Mat smooth, int i, int j){
-        DoubleRawIndexer idx = smooth.createIndexer();
-        double beta = 0;
-        double pc = Math.abs((idx.get(i + 1, j - 1)) - (idx.get(i + 1, j))); // O2 - O1
-        beta += checkConditional(pc);
+        double[] deg = convertToDeg(smooth, i, j);
+        double label = 0;
+        for(int ele = 0; ele < deg.length - 1; ele++){
+            if(Math.abs(subAngles(deg[ele], deg[ele + 1]))> 90){
+                deg[ele+1]+= 180;
+            }
+            label += subAngles(deg[ele], deg[ele+1]);
+        }
+        if (label <= (-180 + 1 )&& label >= (-180 - 1 )){
+            return 1.0;
+        }
+        return 0.0 ;
         
-        pc = Math.abs((idx.get(i, j - 1)) - (idx.get(i + 1, j - 1))); // O3 - O2
-        beta += checkConditional(pc);
-        
-        pc = Math.abs((idx.get(i - 1, j - 1)) - (idx.get(i, j - 1))); //04- O3
-        beta += checkConditional(pc);
-        
-        pc = Math.abs((idx.get(i - 1, j)) - (idx.get(i - 1, j - 1))); // 05 - 04
-        beta += checkConditional(pc);
-        
-        pc = Math.abs((idx.get(i - 1, j + 1)) - (idx.get(i - 1, j))); // 06 - 05
-        beta += checkConditional(pc);
-        
-        pc = Math.abs((idx.get(i, j + 1)) - (idx.get(i - 1, j + 1))); // 07 - 06
-        beta += checkConditional(pc);
-        
-        pc = Math.abs((idx.get(i + 1, j + 1)) - (idx.get(i, j + 1))); // 08 - 07
-        beta += checkConditional(pc);
-       
-        beta /= ( 2 * CV_PI);
-        return beta;   
-        
-    }
-    
-    public double checkConditional(double pc){
-        double beta = 0;
-        if(Math.abs(pc) < CV_PI / 2)
-            beta = pc;
-        else if(pc < -CV_PI /2)
-            beta = CV_PI + pc;
-        else beta = CV_PI - pc;
-        return beta;
     }
 
+    public Mat convertToDegrees(Mat radians){
+        Mat degree = radians.clone();
+        DoubleRawIndexer rad = radians.createIndexer();
+        DoubleRawIndexer deg = degree.createIndexer();
+        for(int i = 0; i < radians.rows(); i++){
+            for(int j = 0; j < radians.cols(); j++){
+                double d = Math.toDegrees(rad.get(i, j));
+                if(d < 0){
+                    d = 180 + d;
+                }
+                deg.put(i, j, d);
+            }
+        }
+        return degree;
+    }
+    
+    public double subAngles(double x, double y){
+        double tmp = x - y;
+        if(Math.abs(tmp) > 180){
+            tmp =  -1 * Math.signum(tmp) * (360 - Math.abs(tmp));
+        }
+        return tmp;
+    }
+    
+    public double[] convertToDeg(Mat radians, int x, int y){
+        
+        int[][] neg = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
+        double[] deg = new double[9];
+        DoubleRawIndexer rad = radians.createIndexer();
+        for( int i = 0; i < neg.length; i++){
+            double d = Math.toDegrees(rad.get(x - neg[i][0], y - neg[i][1]));
+            if(d < 0){
+                d = 180 + d;
+            }
+            deg[i] = d;
+
+        }
+        return deg;
+    }
 }
